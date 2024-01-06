58c197e34380ec072ed215f41b010059aeb2a29b
Petar Atanasov <149053952+petar-Atanasov@users.noreply.github.com>
2024-01-06 14:00:04 +0000

    Update coursework1.cpp

--- coursework1.cpp
+++ coursework1.cpp
@@ -5,11 +5,19 @@ M00916537
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <ctime>
 
 
 class Book {
+    
+    private: 
+    int bookID;
+    std::string bookName;
+    std::string authorFirstName;
+    std::string authorLastName;
+    std::string bookType;
 
-
+  
 
 };
 
