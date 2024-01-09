e6e61c9b45dfb5a863b92adcea0affe6da3c65aa
Petar Atanasov <149053952+petar-Atanasov@users.noreply.github.com>
2024-01-09 04:06:59 +0000

    Add files via upload

---
+++ book.cpp
@@ -0,0 +1,51 @@
+#include <book.h>
+using namespace std;
+
+// pass the constructor 
+Book::Book(int bookID, string bookName, string authorFirstName,
+    string authorLastName){
+
+    // initilize member variables to default value for the cpp file
+    bookID = 0;
+    bookName = "";
+    authorFirstName = "";
+    authorLastName = "";
+    bookType = "";
+    dueDate = "";
+    // to add borrower from Member class
+
+    //getter methods from book.h
+    string Book::getbookID(){
+        return bookID;
+    }
+
+    string Book::getbookName(){
+        return bookName;
+    }
+
+    string Book::getAuthorFristName(){
+        return authorFirstName;
+    }
+
+    string Book::getAuthorLastName(){
+        return authorLastName;
+    }
+
+    string Book::getDueDate(){
+        return dueDate;
+    }
+
+    // setter methods from book.h
+    void Book::setDueDate(){
+        dueDate = date;
+    }
+
+    // other methods from book.h
+    void Book::returnBook(){
+        dueDate = "";
+        // to put the borrowed member here
+    }
+
+
+}
+    
---
+++ book.h
@@ -0,0 +1,36 @@
+#pragma once 
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+class Book {
+    // private data members for the Book class
+    private: 
+    int bookID;                 // book ID
+    string bookName;            // Name of the book
+    string authorFirstName;     // Author First Name
+    string authorLastName;      // Author Last Name 
+    string bookType;            // The book type#
+    string dueDate;             // The due date of the book
+    //Member borrower;
+
+    public:
+    // get constructor and initilize book objects
+    Book(int bookID, string bookName, string authorFirstName,
+    string authorLastName);
+
+    // getter methods 
+    string getbookID();
+    string getbookName();
+    string getAuthorFristName();
+    string getAuthorLastName();
+    string getDueDate();
+
+    // setter methods 
+    void setDueDate(string date);
+
+    // other methods
+    void returnBook();          //Return the occupied book
+    void borrowBook();          //Borrowing a book
+};
