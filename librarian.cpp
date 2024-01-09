fa2734f84f219355cdfff8ff9bcd55fb29154739
Petar Atanasov <149053952+petar-Atanasov@users.noreply.github.com>
2024-01-09 17:14:50 +0000

    Update librarian.cpp

---
+++ librarian.cpp
@@ -0,0 +1,14 @@
+#include "librarian.h"
+using namespace std;
+
+// pass the constructor
+Librarian::Librarian(int staffID, string name, string address,
+    string email, int salary){
+    
+    staffId = 0;
+    salary = 0;
+
+
+    // other metrhods 
+    void  Librarian::issueBook(int Member memberID,) 
+    }
