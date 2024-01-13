5955ae01e71de88e602a5501c9d80ceb3f5d08b6
Petar Atanasov <149053952+petar-Atanasov@users.noreply.github.com>
2024-01-13 01:49:17 +0000

    update main

--- main.cpp
+++ main.cpp
@@ -7,24 +7,11 @@ M00916537
 #include <string>
 #include <ctime>
 #include <vector>
-#include "book.h"
-#include "member.h"
-#include "librarian.h"
-#include "person.h"
 
 using namespace std;
 
 int main(){
     
-    ifstream file("library_book.csv");
-    if(file.is_open()){
-        cout << "The file is open."
-    }
-    else {
-        file.fail();
-        cout << "The file failed to be open"
-    }
-
     
     return 0;
 };
