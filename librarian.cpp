6bfc77eccc45f7b9830f9a48331b81dec2043b93
Petar Atanasov <149053952+petar-Atanasov@users.noreply.github.com>
2024-01-13 22:44:15 +0000

    implimented calcFine

--- librarian.cpp
+++ librarian.cpp
@@ -15,13 +15,50 @@ Librarian::Librarian(int staffID, string name, string address, string email, int
     this->salary = salary;
 }
 // other methods from librarian.h
-void Librarian::issueBook(int memberID, int bookID);
-void Librarian::returnBook(int memberID, int bookID);
-void Librarian::calcFine(int memberID);
+void Librarian::issueBook(int memberID, int bookID) {}
+void Librarian::returnBook(int memberID, int bookID) {}
+void Librarian::calcFine(int memberID)
+{
+    // check for valid member id
+    if (memberID > 0 || memberID >= members.size())
+    {
+        cout << "This is an invalid member id: " << memberID << ". Try again!" << endl;
+
+        return;
+    }
+
+    vector<Book *> books = members[memberID].getBooksBorrowed();
+    // variable for the total amount of the fine
+    double toFis = 0.0;
+
+    for (auto &book : books)
+    {
+        // get the current time with chrono + ctime
+        auto currTime = chrono::system_clock::now();
+        // set the date up to date converted with to_time_t from chrono
+        time_t currDate = chrono::system_clock::to_time_t(currTime);
+
+        // chech for the delay of the book
+        if (currDate > book->getDueDate())
+        {
+            double delayDate = difftime(currDate, book->getDueDate()) / (3600 * 24);
+            double fnFBk = delayDate * 1.0;
+            // pass the variable for total amount and add it with fine for book 
+            toFis += fnFBk;
+
+            cout << "The expired time to return the book is: " << delayDate
+                 << " and the applied fine for the member with ID: " << memberID
+                 << " is: £" << toFis << '\n';
+        }
+        else
+        {
+            cout << "The book has been returned succsessfully on time. "
+                 << "You do not have applied fine.\n";
+        }
+    }
+}
 void Librarian::addMember(int memberID, string name, string address, string email)
 {
-    // assign the size of members() vector to the variable memberID
-    //  memberID = members().size();
     // setName from person.cpp to get the name
     setName(name);
     // setAddress form person.cpp to get the address
@@ -32,7 +69,6 @@ void Librarian::addMember(int memberID, string name, string address, string emai
     members.emplace_back(memberID, name, address, email);
     std::cout << "The member ID is: " << memberID << endl;
 }
-
 void Librarian::displayBorrowedBooks(int memberID)
 {
     // pass the vector for books from librarian.h
