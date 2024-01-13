d8be9bc21144cfc1d0cc797d24695fc39083d86f
Petar Atanasov <149053952+petar-Atanasov@users.noreply.github.com>
2024-01-13 07:16:27 +0000

    create setName, setAddress and create a regex for setEmail

--- person.cpp
+++ person.cpp
@@ -3,10 +3,11 @@ Petar Atanasov
 M00916537
 */
 #include "person.h"
+#include <regex>
 using namespace std;
 
-// pass the default constructor 
-Person::Person(){}
+// pass the default constructor
+Person::Person() {}
 
 // pass the parameterized constructor
 Person::Person(string name, string address, string email) {}
@@ -14,15 +15,33 @@ Person::Person(string name, string address, string email) {}
 // setter methods from person.h
 void Person::setName(string nm)
 {
-    this->name = nm;
+    cout << "Enter your name: " << endl;
+    cin >> nm;
+    name = nm;
 }
 void Person::setAddress(string adrs)
 {
-    this->address = adrs;
+    cout << "Enter your address: " << endl;
+    cin >> adrs;
+    address = adrs;
 }
 void Person::setEmail(string eml)
 {
-    this->email = eml;
+    do
+    {
+        cout << "Enter your email address: " << endl;
+        cin >> eml;
+
+        regex rgxEml(("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+"));
+
+        if (!regex_match(eml, rgxEml))
+        {
+            cout << "The entered email was wrong! Please try again: " << endl;
+            cin >> eml;
+        }
+    } while (!regex_match(eml, rgxEml));
+
+    email = eml;
 }
 
 // getter methods from person.h
